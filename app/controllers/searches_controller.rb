475029cc3e316e600926d5bff350aa177a610a06
Ben Hughes <me@benhughes.name>
2026-01-04 15:47:21 -0800

    Fix issue with linked searches and CGI escaping

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -8,13 +8,13 @@ class SearchesController < ApplicationController
   def show
     params[:query] = params[:id]
 
-    @search = Search.new(params[:id])
+    @search = Search.new(CGI.unescape(params[:id]))
     @results = @search.all
 
     respond_with @results do |format|
       format.html do
         if @results.size == 0
-          redirect_to root_path, alert: "Sorry, but no results were found."
+          redirect_to root_path, alert: 'Sorry, but no results were found.'
         elsif @results.size == 1
           redirect_to path_to_searchable(@results.first)
         end
@@ -26,5 +26,4 @@ class SearchesController < ApplicationController
     params[:id] = params[:q]
     show
   end
-
 end
